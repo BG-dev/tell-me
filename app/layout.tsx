c78340caa735d8a964eaf75e8a94b58f92a221d2
nikita.azizov <nik.azizoff2016@gmail.com>
2025-01-05 00:45:30 +0300

    feat: add root layout with sidenav

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 import Sidenav from "./_components/Sidenav";
 
 const geistSans = Geist({
