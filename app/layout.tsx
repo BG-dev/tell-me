c0d50c4e927840e79f330f17ad8c8927148763a0
nikita.azizov <nik.azizoff2016@gmail.com>
2025-01-07 12:00:00 +0300

    refactor: remove import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
 import Sidenav from "./_components/Sidenav";
 
 const geistSans = Geist({
