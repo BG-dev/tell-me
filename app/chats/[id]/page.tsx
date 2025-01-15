0913e64b2244a45759a238126e8f083a6f9259d1
nikita.azizov <nik.azizoff2016@gmail.com>
2025-01-15 18:00:00 +0300

    feat: add dynamic chat page

--- app/chats/[id]/page.tsx
+++ app/chats/[id]/page.tsx
@@ -1,3 +1,8 @@
-export default function Page() {
-  return <div>Chat </div>;
+type PageProps = {
+  params: Promise<{ id: string }>;
+};
+
+export default async function Page({ params }: PageProps) {
+  const id = (await params).id;
+  return <div>Chat {id}</div>;
 }
